35cf45ccbee60bb6c1b25c36eadc84957d2c2792
nadina <nadinasiddiqui@msn.com>
2019-08-31 20:17:42 +0200

    remove backend

--- backend/error.py
+++
@@ -1,9 +0,0 @@
-
-class Error:
-    def __init__(self,
-                 message='',
-                 old_location='',
-                 new_location='',
-                 series_name='',
-                 e_nr='',
-                 s_nr='')
